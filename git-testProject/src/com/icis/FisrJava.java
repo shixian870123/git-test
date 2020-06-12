30dc59f4b86ac9f40b6d1ed1f0d8da4dc0671a76
shixian <13521348721@163.com>
2020-06-12 16:58:21 +0800

    把git-testProject工程推送到本地版本库

---
+++ git-testProject/src/com/icis/FisrJava.java
@@ -0,0 +1,13 @@
+package com.icis;
+
+public class FisrJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+    }
+}
